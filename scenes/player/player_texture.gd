3a531ca2e8297f06e5b3be55d6db25ba90d4622a
pszanetti <p.s.zanetti@hotmail.com>
2026-09-16 20:42:56 -0300

    Pulo, queda, fall, exceto a pausa no landing

--- scenes/player/player_texture.gd
+++ scenes/player/player_texture.gd
@@ -5,22 +5,41 @@ class_name PlayerTexture
 # Aqui eu pego o path e altero a variável animation para guardar o 
 # Node cujo caminho estava descrito animation e guardo como Node tipo AnimationPlayer
 
+# Guarda o acesso ao node Animation na variável animation que fica sendo o AnimationPlayer
 export(NodePath) onready var animation = get_node(animation) as AnimationPlayer
 
+# Guarda o acesso ao node Player na variável player para acessar suas variáveis e funções
+export(NodePath) onready var player = get_node(player) as KinematicBody2D
 
-# Revebe o valor de velocity que está no script do Node raiz (Player) e armazena
+# Recebe o valor de velocity que está no script do Node raiz (Player) e armazena
 # na variável direction
 func animate(direction: Vector2) -> void:
 # print(direction) checar se está recebendo o velocity
+
 	verify_direction(direction)
-	horizontal_behaviour(direction)
+	
+	if direction.y != 0:
+		vertical_behaviour(direction)
+	elif player.landing:
+		animation.play("landing")
+	else:
+		horizontal_behaviour(direction)
 
 func verify_direction(direction: Vector2) -> void:
 	if direction.x > 0:
 		flip_h = false
 	elif direction.x < 0:
 		flip_h = true
-	pass
+
+func vertical_behaviour(direction: Vector2) -> void:
+	# Se direção de y > 0 está caindo, animação fall
+	if direction.y > 0:
+		player.landing = true
+		animation.play("fall")
+	# Se direção de y < 0 está pulando, animação jump
+	elif direction.y < 0:
+		animation.play("jump")
+
  
 func horizontal_behaviour(direction: Vector2) -> void:
 	if direction.x != 0:
@@ -28,3 +47,10 @@ func horizontal_behaviour(direction: Vector2) -> void:
 	else:
 		animation.play("idle")
 	
+
+
+func _on_animation_finished(anim_name: String) -> void:
+	match anim_name:
+		"landing":
+			player.landing = false
+

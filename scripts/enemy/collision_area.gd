4c39bcad79e2cc5af09ceb68671164f2c62c2e65
pszanetti <p.s.zanetti@hotmail.com>
2026-09-19 16:53:54 -0300

    Configurano código para atualizar a vida do inimido em EnemyTemplate

---
+++ scripts/enemy/collision_area.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+class_name CollisionArea
+
+onready var timer: Timer = get_node("Timer")
+
+export(int) var health
+export(float) var invulnerability_timer
+
+# Cria o link para acesso a dados do "Pai" -> KinemticBody EnemyTemplate
+export(NodePath) onready var enemy = get_node(enemy) as KinematicBody2D
+
+
+func on_collision_area_entered(area):
+	# Se o pai da área de ataque for o Player
+	if area.get_parent() is Player:
+		# Vai para o pai da area, ou seja o Player e pega p node filho Stats
+		var player_stats: Node = area.get_parent().get_node("Stats")
+		var player_attack = player_stats.base_attack + player_stats.bonus_attack
+		update_health(player_attack)
+	
+# Recebe o dano player_attack como damage
+func update_health(damage: int) -> void:
+	health -= damage
+	if health <= 0:
+		# Acessa a raiz e coloca como true sua variável caan_die
+		enemy.can_die = true
+		return # -> Sai da função
+	
+	enemy.can_hit = true
+

d08b87c37ee31ab372af6666cbe15deeca22716c
pszanetti <p.s.zanetti@hotmail.com>
2026-09-22 13:46:25 -0300

    Criação do script do Feitiço  -> 211

---
+++ scripts/player/spell_area.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+class_name FireSpell
+
+var spell_damage: int
+
+func _ready() -> void:
+	for children in get_children():
+		if children is Particles2D:
+			children.emitting = true
+			
+		
+	
+
+
+
+func on_animation_finished(_anim_name: String) -> void:
+	queue_free()
+	

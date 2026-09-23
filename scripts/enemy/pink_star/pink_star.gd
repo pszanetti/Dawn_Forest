c4a841e9184ab718ceca3e57bb104a86e2fbe109
pszanetti <p.s.zanetti@hotmail.com>
2026-09-23 17:57:11 -0300

    Configurando Itens a serem dropados pelo Pink Star - 227

--- scripts/enemy/pink_star/pink_star.gd
+++ scripts/enemy/pink_star/pink_star.gd
@@ -2,3 +2,61 @@ extends EnemyTemplate
 
 class_name PinkStar
 
+func _ready() -> void:
+	randomize()
+	drop_list = {
+		"Heal Potion" : [
+			"res://assets/item/consumable/health_potion.png",
+			25,
+			"Helath",
+			5,
+			2
+			],
+		
+		"Mana Potion" : [
+			"res://assets/item/consumable/mana_potion.png",
+			12,
+			"Mana",
+			5,
+			3
+			],
+		
+		"Pink Star Mouth": [
+			"res://assets/item/resource/pink_star/pink_star_mouth.png",
+			47,
+			"Resource",
+			{},
+			5
+			],
+		
+		"Pink Star Bow" : [
+			"res://assets/item/equipment/pink_star_bow.png",
+			1,
+			"Weapon",
+			{
+				"Attack" : 5
+			},
+			60
+			],
+			
+		"Pink Star Belt" : [
+			"res://assets/item/equipment/pink_star_belt.png",
+			3,
+			{
+				"Health" : 6,
+				"Mana" : 5,
+			},
+			40
+			],
+		"Pink Star Shield" : [
+			"res://assets/item/equipment/pink_star_shield.png",
+			1,
+			"Weapon",
+			{
+				"Health" : 3,
+				"Defense" : 2
+			},
+			75
+			]
+		}
+

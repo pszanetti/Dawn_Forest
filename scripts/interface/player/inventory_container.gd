aa7479335d1bb1f8ef64666aa2cec65f8b0d19ef
pszanetti <p.s.zanetti@hotmail.com>
2026-09-24 14:48:05 -0300

    Script do inventário - Configurando os slots - 235 e 236

--- scripts/interface/player/inventory_container.gd
+++ scripts/interface/player/inventory_container.gd
@@ -27,8 +27,31 @@ func _ready() -> void:
 	for children in slot_container.get_children():
 		children.connect("empty_slot", self, "empty_slot")
 		
+func update_slot(item_name: String, item_image: StreamTexture, item_info: Array) -> void:
+	# Verifica se o item já existe
+	var existing_item_index: int = slot_list.find(item_name)
+	if existing_item_index != -1:  # Encontrou o item !?
+		var item_slot: TextureRect = slot_container.get_child(existing_item_index)
+		if item_slot.amount < 9 and item_slot.item_type != "Equipament" and item_slot.item_type != "Weapon":
+			var current_amount: int = item_slot.amount + item_info[4]
+			if current_amount > 9:
+				# Resto -> leftover
+				var leftover: int = current_amount - 9
+				item_info[3] = 9 - item_slot.amount 
+				item_slot.update_item(item_name, item_image, item_info)
+				item_info[3] = leftover
+				update_slot(item_name, item_image, item_info) 
+				return
+			item_slot.update_item(item_name, item_image, item_info) # Se não exceder 9
+			return
+		pass
+	
+func update_slot() -> void:
+	
+	pass
 	
 	
 func empty_slot(index: int) -> void:
-	
+	slot_list[index] = ""
+	slot_item_info[index] = ""
 	pass
